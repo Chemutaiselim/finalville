a7fa55b3292886c2b21f74ed81dc8c29356305e5
emilio <emilio113kariuki@gmail.com>
2022-03-28 19:31:35 +0300

    March 28, 2022 7:31 PM

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:finalville/screen/Home.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  Firebase.initializeApp( );
   runApp(const GetMaterialApp(
     home: Home(),
     debugShowCheckedModeBanner: false,
