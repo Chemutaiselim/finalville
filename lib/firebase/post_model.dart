2da5f9e6c2ee29489dca4cc504ef17b361bc060d
emilio <emilio113kariuki@gmail.com>
2022-03-28 20:36:05 +0300

    March 28, 2022 8:36 PM

--- lib/firebase/post_model.dart
+++ lib/firebase/post_model.dart
@@ -4,7 +4,11 @@ class PostModel {
   final String location;
   final String image;
   final String type;
+  final double latitude;
+  final double longitude;
   PostModel({
+    required this.latitude,
+    required this.longitude,
     required this.description,
     required this.title,
     required this.location,
@@ -14,11 +18,12 @@ class PostModel {
   factory PostModel.fromRTDB(Map<String, dynamic> data) {
     return PostModel(
       description: data['description'] ?? 'waste',
-      title: data['title']?? 'waste',
+      title: data['title'] ?? 'waste',
       location: data['location'] ?? 'Nyeri',
-      image: data['image'] ??  'image',
+      image: data['image'] ?? 'image',
       type: data['type'] ?? 'type',
-      
+      latitude: data['latitude'] ?? 'latitude',
+      longitude: data['longitude'] ?? 'longitude',
     );
   }
 }
