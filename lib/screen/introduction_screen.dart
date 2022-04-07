c8a22c9bf1821a1eb43301551cd2df356aaf5619
Emilio kariuki <emilio113kariuki@gmail.com>
2022-04-07 23:19:24 +0300

    April 7, 2022 11:19 PM

--- lib/screen/introduction_screen.dart
+++ lib/screen/introduction_screen.dart
@@ -56,13 +56,13 @@ class _IntroductionState extends State<Introduction> {
             PageViewModel(
               title: "Collect",
               body: "We provide a platform for us to collect the waste.",
-              image: introImage('assets/images/man.png'),
+              image: introImage('assets/images/ice-cream.png'),
               decoration: pageDecoration,
             ),
             PageViewModel(
               title: "Recycle",
               body: "We protect the environment though recycling the trash and maintaining a cleaner world.",
-              image: introImage('assets/images/lady.png'),
+              image: introImage('assets/images/Doggie.png'),
               decoration: pageDecoration,
             ),
 
